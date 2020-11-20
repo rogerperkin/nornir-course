ce5acf4186c4e560a714d6fb34ebd242c562931e
Roger <roger@rogerperkin.co.uk>
2020-11-20 12:04:41 -0500

    updated sh ip int script

---
+++ sh-ip-int.py
@@ -0,0 +1,13 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
+from nornir.core.filter import F 
+
+nr = InitNornir(config_file="/home/roger/nornir-course/config.yml")
+
+Router = nr.filter(F(groups__contains="CSR_Routers"))
+
+result = Router.run(netmiko_send_command, command_string="sh version")
+
+print_result(result)
+
